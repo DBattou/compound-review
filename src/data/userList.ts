80bc0453476209a32325ae2049adacaa82119c7f
DBattou <doucerain.b@gmail.com>
2025-04-09 21:07:59 +0200

    import data in separate file

---
+++ src/data/userList.ts
@@ -0,0 +1,23 @@
+const USER_LIST: Array<string> = [
+  'Mackenzie MacGyver',
+  'Darien Kassulke',
+  'Jewel Wisoky',
+  'Clyde Prosacco',
+  'Ralph Thiel',
+  'Kaylah Spencer',
+  'Norma Bernhard',
+  'Donnie Oberbrunner',
+  'Patience Smith',
+  'Name',
+  'Mackenzie MacGyver',
+  'Darien Kassulke',
+  'Jewel Wisoky',
+  'Clyde Prosacco',
+  'Ralph Thiel',
+  'Kaylah Spencer',
+  'Norma Bernhard',
+  'Donnie Oberbrunner',
+  'Patience Smith',
+];
+
+export default USER_LIST;
