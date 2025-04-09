cd62df3323d012aab6063e3ae5cb89033ab86324
DBattou <doucerain.b@gmail.com>
2025-04-09 21:45:22 +0200

    add margin to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export default function Header({ onAddContact }: HeaderProps) {
   return (
-    <div className="flex flex-row place-content-between">
+    <div className="flex flex-row place-content-between pb-8">
       <h1>100 Contacts</h1>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
