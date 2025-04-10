25d85dc9d9807f1ab6e35a04b246ef5dca707e7f
DBattou <doucerain.b@gmail.com>
2025-04-10 14:35:29 +0200

    add padding to the text

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -7,7 +7,7 @@ interface ContactListProps {
 export default function ContactList({ contacts }: ContactListProps) {
   return (
     <div className="flex flex-col items-start bg-gray-800 border-1 border-gray-500 rounded-lg p-3">
-      <p>Name</p>
+      <p className="py-4 pl-3">Name</p>
       <hr className="bg-gray-500 w-full" />
       {contacts.map((contact) => {
         return (
