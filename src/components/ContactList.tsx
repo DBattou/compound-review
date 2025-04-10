1ed6797e64db720fa4ba004b74553e0aa6430233
DBattou <doucerain.b@gmail.com>
2025-04-10 15:20:27 +0200

    move the hr element on top of name

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -8,12 +8,11 @@ export default function ContactList({ contacts }: ContactListProps) {
   return (
     <div className="flex flex-col items-start bg-gray-800 border-1 border-gray-500 rounded-lg p-3">
       <p className="py-4 pl-3">Name</p>
-      <hr className="bg-gray-500 w-full" />
       {contacts.map((contact) => {
         return (
           <React.Fragment key={contact}>
-            <p className="text-blue-500 py-4 pl-3">{contact}</p>
             <hr className="bg-gray-500 w-full" />
+            <p className="text-blue-500 py-4 pl-3">{contact}</p>
           </React.Fragment>
         );
       })}
