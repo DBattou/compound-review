d9d2953aed53d8d86c3c0109610d7a30a3581126
DBattou <doucerain.b@gmail.com>
2025-04-10 15:20:48 +0200

    reduce contact list size

--- src/data/userList.ts
+++ src/data/userList.ts
@@ -2,13 +2,9 @@ const USER_LIST: Array<string> = [
   'Mackenzie MacGyver',
   'Darien Kassulke',
   'Jewel Wisoky',
-  'Clyde Prosacco',
-  'Ralph Thiel',
-  'Kaylah Spencer',
-  'Norma Bernhard',
-  'Donnie Oberbrunner',
-  'Patience Smith',
-  'Name',
+];
+
+export const FULL_USER_LIST: Array<string> = [
   'Mackenzie MacGyver',
   'Darien Kassulke',
   'Jewel Wisoky',
