78d421c67096839f892b9c053acac21d450ba116
DBattou <doucerain.b@gmail.com>
2025-04-10 20:22:49 +0200

    handle empty contact list

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -8,14 +8,18 @@ export default function ContactList({ contacts }: ContactListProps) {
   return (
     <div className="flex flex-col items-start bg-gray-800 border-1 border-gray-500 rounded-lg p-3">
       <p className="py-4 pl-3">Name</p>
-      {contacts.map((contact) => {
-        return (
-          <React.Fragment key={contact}>
-            <hr className="bg-gray-500 w-full" />
-            <p className="text-blue-500 py-4 pl-3">{contact}</p>
-          </React.Fragment>
-        );
-      })}
+      {contacts.length === 0 ? (
+        <p className="py-4">There is currently no contact in your list.</p>
+      ) : (
+        contacts.map((contact) => {
+          return (
+            <React.Fragment key={contact}>
+              <hr className="bg-gray-500 w-full" />
+              <p className="text-blue-500 py-4 pl-3">{contact}</p>
+            </React.Fragment>
+          );
+        })
+      )}
     </div>
   );
 }
