5855915e78cec5043d2fbdbd8aa9bb9fac46a989
DBattou <doucerain.b@gmail.com>
2025-04-11 16:35:40 +0200

    Add contactDetails component

---
+++ src/components/ContactDetails.tsx
@@ -0,0 +1,31 @@
+import { USER_LIST } from '../data/userList';
+
+interface ContactDetailsProps {
+  onClose: () => void;
+  contactId: string | undefined;
+}
+
+export default function ContactDetails({ onClose, contactId }: ContactDetailsProps) {
+  const contactData = USER_LIST.find(({ id }) => id === contactId);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center p-5 z-50">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      <div className="relative min-w-[440px] flex flex-col items-start rounded-lg bg-neutral-800 ">
+        <div className="flex flex-row place-content-between w-full items-center p-2">
+          <p className="text-white h-full">{`${contactData?.firstName} ${contactData?.lastName}`}</p>
+          <button
+            className="text-white"
+            onClick={onClose}
+            aria-label="Close contact details"
+          >
+            x
+          </button>
+        </div>
+        <p className="text-white h-full p-4">Phone: {contactData?.phoneNumber}</p>
+        <p className="text-white h-full p-4">Address: {contactData?.address}</p>
+      </div>
+    </div>
+  );
+}
